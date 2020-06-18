68fbfd8c46fea6b7348618f1ef643c947d7ad3b1
saihaj <44710980+saihaj@users.noreply.github.com>
2020-06-18 14:54:49 -0500

    build: update dependencies
    Also fix linting errors

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react'
 import { Fixed } from '@primer/components'
 import { AnimatePresence, motion } from 'framer-motion'
